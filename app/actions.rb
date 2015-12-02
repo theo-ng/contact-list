60bccd7a5361f23a73a2dc17ae335c5d246caaab
Theo <theo-ng@users.noreply.github.com>
2015-12-01 16:55:40 -0800

    list and new completed

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,16 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  Contact.all.order(:id).to_json
+end
+
+post '/contacts/new' do
+  contact = Contact.new(params)
+  if contact.save
+    contact.to_json
+  else
+    halt(500)
+  end
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,40 @@
 $(function() {
+  var $contacts = $('#contacts')
+  var handlers = {
+    toggleNew: function() {
+      $('#newContact').toggle();
+    },
+    toggleList: function() {
+      $contacts.toggle();
+    },
+    addToList: function (contact) {
+      $contact = $('<li>');
+      $contact.text("Name: " + contact.firstname + " " + contact.lastname + " Email: " + contact.email);
+        $contact.appendTo($contacts);
+    },
+    getContacts: function(contacts) {
+      $.each(contacts, function(i, contact) {
+        handlers.addToList(contact);
+      });
+    },
+  };
 
-  $('#btn-new').on("click", function() {
-    $('.new').text('new stuff');
+  $('#btn-new').on("click", handlers.toggleNew);
+
+  $('#btn-list').on("click", handlers.toggleList);
+
+  $('#btn-new-contact').on("click", function() {
+    var fName = $('#firstName').val();
+    var lName = $('#lastName').val();
+    var email = $('#email').val();
+    var contact = { firstname: fName, lastname: lName, email: email }
+
+    $.post("/contacts/new", contact, function(data) {
+      if(data) {
+        handlers.addToList(data);
+      }
+    }, 'json');
   });
 
+  $.getJSON("/contacts", handlers.getContacts);
 });
